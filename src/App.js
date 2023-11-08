1c8d7a80f8a65dc8a4f32dbc2a39c9b7e96b6268
Manash Debnath <itsmanash1999@gmail.com>
2023-11-09 01:18:03 +0530

    Hero-Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Navbar from './Component/Navbar/Nav'
+import Herosection from './Component/Hero_Section'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Herosection/>
+      
     </>
   );
 }
---
+++ src/Component/Hero_Section/index.js
@@ -0,0 +1,16 @@
+import {ReactComponent} from './vibrating-headphone 1.svg'
+import './index.css'
+
+export default function heroSection(){
+    return (
+        <div id='S1'>
+            <div id='text'>
+                <div>100 Thousand Songs, ad-free</div>
+                <div>Over thousands podcast episodes</div>
+            </div>
+            <div id='headphn-logo'>
+                <ReactComponent/>
+            </div>
+        </div>
+    )
+}
